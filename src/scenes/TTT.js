f9831ac6b5f3035344dae34033baf88a0bab4974
Ernany Gonçalves <goncalves.ernany@gmail.com>
2020-03-19 12:11:48 -0300

    Tabuleiro agora aparece de fundo

--- src/scenes/TTT.js
+++ src/scenes/TTT.js
@@ -7,36 +7,37 @@ export default class TTT extends Phaser.Scene {
     create() {
         this.add.text(30, 30, "GAME!", { font: "25px 'Titan One'", fill: "red" });
 
-        this.ttt_x = this.add.sprite(200,200,"x");
 
-            // https://photonstorm.github.io/phaser3-docs/Phaser.Types.Animations.html#.GenerateFrameNames__anchor
-            // https://phaser.discourse.group/t/texture-atlas-anim-issue-with-generateframenumbers/1264/2   
-            // Acredito que generateFrameNames pega pelo json os "nomes dos arquivos" que compõe o spridesheet e gera os frames
+        this.add.image(0, 0, 'board').setOrigin(0, -0.25);
 
-            this.anims.create({
-                key:"x_clicked",
-                frames: this.anims.generateFrameNames("x"),
-                frameRate:20,
-                repeat:0
-            });
+        this.ttt_x = this.add.sprite(200, 200, "x");
 
-            this.ttt_x.play("x_clicked");
+        // https://photonstorm.github.io/phaser3-docs/Phaser.Types.Animations.html#.GenerateFrameNames__anchor
+        // https://phaser.discourse.group/t/texture-atlas-anim-issue-with-generateframenumbers/1264/2   
+        // Acredito que generateFrameNames pega pelo json os "nomes dos arquivos" que compõe o spridesheet e gera os frames
 
-            this.ttt_o = this.add.sprite(300,300,"o");
+        this.anims.create({
+            key: "x_clicked",
+            frames: this.anims.generateFrameNames("x"),
+            frameRate: 20,
+            repeat: 0
+        });
 
-            this.anims.create({
-                key:"o_clicked",
-                frames: this.anims.generateFrameNames("o"),
-                frameRate:20,
-                repeat:0
-            });
+        this.ttt_x.play("x_clicked");
 
-            this.ttt_o.play("o_clicked");
+        this.ttt_o = this.add.sprite(300, 300, "o");
+
+        this.anims.create({
+            key: "o_clicked",
+            frames: this.anims.generateFrameNames("o"),
+            frameRate: 20,
+            repeat: 0
+        });
+
+        this.ttt_o.play("o_clicked");
 
 
-        this.add.image(0, 0, 'board').setOrigin(0,-0.25);
-
-        this.buttonStart = this.add.bitmapText(50,50,"bitmap_font","1234567890ABCDEabcde",40);
+        this.buttonStart = this.add.bitmapText(50, 50, "bitmap_font", "1234567890ABCDEabcde", 40);
 
         setTimeout(() => {
             this.scene.start("endgame");
